2b95b04b8bcdbc6ddbe488bc41dc67f7953ae9cd
Kristian Soelling <kristiansoelling@gmail.com>
2019-04-29 14:48:59 +0200

    Add core gems and create components page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :about]
+  skip_before_action :authenticate_user!, only: %i[home about]
   def home; end
 
   def about; end
+
+  def components; end
 end
--- app/models/contact_request.rb
+++ app/models/contact_request.rb
@@ -1,9 +1,20 @@
+# == Schema Information
+#
+# Table name: contact_requests
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ContactRequest < ApplicationRecord
   validates :email,
-            presence: :true,
+            presence: true,
             format: {
               with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
               message: "must be a valid email address"
             }
-  validates :content, presence: :true, length: {minimum: 10, maximum: 1000}
+  validates :content, presence: true, length: { minimum: 10, maximum: 1000 }
 end
