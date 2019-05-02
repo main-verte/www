fc087b2a4b6fdbc4876b3e574f2ef787d2b8185f
Hadrien Matringe <hadrien.matringe@gmail.com>
2019-05-02 14:54:17 +0200

    Simple auth in staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+
+  if Rails.env.production? && ENV['STAGING']
+    http_basic_authenticate_with name: ENV['STAGING_BASIC_AUTH_NAME'], password: ENV['STAGING_BASIC_AUTH_PWD']
+  end
 end
