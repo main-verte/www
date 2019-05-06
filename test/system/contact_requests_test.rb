ab2de7689f4c08efaff302aa13204947460412ea
Kristian Soelling <kristiansoelling@gmail.com>
2019-05-06 10:27:02 +0200

    Add testing for ContactRequest validators and website form interaction

--- test/system/contact_requests_test.rb
+++ test/system/contact_requests_test.rb
@@ -1,21 +1,6 @@
 require "application_system_test_case"
 
 class ContactRequestsTest < ApplicationSystemTestCase
-  test "should not save ContactRequest instance without email and content input entirely" do
-    contact = ContactRequest.new
-    assert_not contact.save, "Saved a ContactRequest instance without input entirely"
-  end
-
-  test "should not save ContactRequest instance without proper email credentials" do
-    contact = ContactRequest.new(email: 'testtest.com', content: 'This is a test!')
-    assert_not contact.save, "Saved a ContactRequest instance without email credentials"
-  end
-
-  test "should not save ContactRequest instance with fewer than 10 characters for content attribute" do
-    contact = ContactRequest.new(email: 'test@test.com', content: 'Test')
-    assert_not contact.save, "Saved a ContactRequest instance with less than 10 characters"
-  end
-
   test "submitting the ContactRequest form should redirect to root" do
     visit "/"
     fill_in "contact_request_email", with: "test@test.com"
