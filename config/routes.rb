18a143613ecc3eb0ec90b52d6f457e50470fdbc2
Hadrien Matringe <hadrien.matringe@gmail.com>
2019-05-06 13:30:33 +0200

    Setup Cloudinary

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /fr|da/ do
     devise_for :users
     root to: 'pages#home'
-    get 'pages/about'
-    get 'pages/components'
+    get 'about', to: 'pages#about'
+    get 'components', to: 'pages#components'
     resources :contact_requests, only: [:new, :create]
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
